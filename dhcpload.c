5f089aff89577d34d95d3367128161f50e3682fd
John Kelly <jkelly@aircell.com>
2014-06-20 00:45:20 -0600

    Draft 2

--- dhcpload.c
+++ dhcpload.c
@@ -19,17 +19,31 @@
 
 void usage(char *progname) {
 	printf("Usage:  %s [options]\n", progname);
+	printf("\t-i|--interface        Interface (eth0)\n");
+	printf("\t-t|--total            Total requests to generate (1)\n");
+	printf("\t-w|--wait             Seconds of delay between requests (1)\n");
+	printf("\t-j|--concurrent       Number of concurrent sessions (3)\n");
+	printf("\t-d|--debug            Turn on debugging\n");
 	printf("\t-h\t--help\tHelp (this message\n");
 }
 
+ 
+typedef enum {
+	SESS_START, SESS_DISCOVER, SESS_REQUEST, SESS_DONE
+} session_state_t;
 
-unsigned char  snd_buf[512];
-unsigned char  psh_buf[512];
-unsigned char  rcv_buf[2048];
-
+typedef struct {
+	unsigned char   index;
+	session_state_t state;
+	unsigned char   server_mac_addr[ETH_ALEN];
+	unsigned char   client_mac_addr[ETH_ALEN];
+	unsigned char   client_ip_addr[4];
+	struct timeval  tv_discover, tv_offer, tv_request, tv_ack;
+} session_t;
+	
 typedef struct sockaddr_ll sll_t;
 typedef struct sockaddr    sa_t;
-typedef unsigned short us_t;
+typedef unsigned short     us_t;
 
 struct dhcp_body {
 	unsigned char   bootreq, hwtype, alen, hops;
@@ -50,14 +64,27 @@ struct dhcp_packet {
 	struct udphdr        udp;
 	struct dhcp_body     dhcp;
 } __attribute__ ((__packed__));
+typedef struct dhcp_packet dhcp_t;
 
+
+unsigned char  snd_buf[512];
+unsigned char  rcv_buf[2048];
+session_t     *session;
+unsigned int   opt_max = 1;
+unsigned int   opt_wait = 1;
+unsigned int   opt_debug = 0;
+unsigned int   opt_concurrent = 5;
+
+unsigned int   sessions_available;
+unsigned int   sessions_pending;
+unsigned int   sessions_remaining;
 
 void dump_packet(unsigned char *buf, ssize_t cnt) {
 
 	ssize_t              ndx;
-	struct dhcp_packet  *p;
+	dhcp_t              *p;
 
-	p = (struct dhcp_packet *)buf;
+	p = (dhcp_t *)buf;
 
 	printf ("CLIENT MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
 		p->dhcp.addr[0],
@@ -108,7 +135,7 @@ int bind_interface(int sock, char *name) {
 		return -1;
 	}
 
-	printf("Index is %d\n", ifr.ifr_ifindex);
+	if(opt_debug) printf("Interface %s Index is %d\n", name, ifr.ifr_ifindex);
 	sll.sll_family = AF_PACKET;	
 	sll.sll_protocol = htons(ETHERTYPE_IP);
 	sll.sll_ifindex  = ifr.ifr_ifindex;
@@ -124,24 +151,20 @@ int bind_interface(int sock, char *name) {
 	return ifr.ifr_ifindex;
 }
 
-int send_packet(int sock, int type) {
-	struct dhcp_packet     *p;
-	struct dhcp_packet     *r;
+int send_packet(int sock, session_t *sess, int type) {
+	dhcp_t                 *p;
+	dhcp_t                 *r;
 	static unsigned short  idnum = 0x66;
 	ssize_t                len;
 	ssize_t                ndx;
+	socklen_t              slen;
 	
 	memset(snd_buf, 0, sizeof(snd_buf));
-	p = (struct dhcp_packet  *) snd_buf;
-	r = (struct dhcp_packet  *) rcv_buf;
+	p = (dhcp_t  *) snd_buf;
+	r = (dhcp_t  *) rcv_buf;
 
 	memset(&p->eth.ether_dhost, 0xff, ETH_ALEN);
-	p->eth.ether_shost[0] = 0xCC;
-	p->eth.ether_shost[1] = 0xCC;
-	p->eth.ether_shost[2] = 0xCC;
-	p->eth.ether_shost[3] = 0x04;
-	p->eth.ether_shost[4] = 0x05;
-	p->eth.ether_shost[5] = 0x06;
+	memcpy(p->eth.ether_shost, sess->client_mac_addr, ETH_ALEN);
 	p->eth.ether_type = htons(ETHERTYPE_IP);
 
 	p->ip.version = 4;
@@ -158,7 +181,7 @@ int send_packet(int sock, int type) {
 	p->dhcp.bootreq = 1;
 	p->dhcp.hwtype = 1;
 	p->dhcp.alen = ETH_ALEN;
-	p->dhcp.id = 0xCAFE;
+	p->dhcp.id = 0xFECA + (sess->index<<16) + (type<<24);
 	p->dhcp.flags = 0x0080;
 	memcpy(p->dhcp.addr, p->eth.ether_shost, ETH_ALEN);
 	p->dhcp.cookie = htonl(0x63825363);
@@ -209,11 +232,13 @@ int send_packet(int sock, int type) {
 	return write(sock, p, len);
 }
 
+#define TRACE if(opt_debug) printf("%s %d (%d,%d,%d)\n", __func__, __LINE__, sessions_remaining, sessions_pending, sessions_available)
+#define DETAIL if(opt_debug) printf("Found %d/%d: %s %d (%d,%d,%d)\n",found, session[found].state, __func__, __LINE__, sessions_remaining, sessions_pending, sessions_available)
 int main(int argc, char **argv) {
 
 
 	int         packet_socket;
-	int         rc, ch, cnt;
+	int         rc, ch, cnt, ndx;
 	sll_t       sll;         
 	ssize_t     rcnt;
 	socklen_t   slen;
@@ -222,12 +247,28 @@ int main(int argc, char **argv) {
 	/* options descriptor */
 	static struct option longopts[] = {
 		{ "help",       no_argument,            NULL,           'h' },
+		{ "debug",      no_argument,            NULL,           'h' },
+		{ "wait",       required_argument,      NULL,           'w' },
+		{ "total",      required_argument,      NULL,           't' },
+		{ "concurrent", required_argument,      NULL,           'j' },
 		{ "interface",  required_argument,      NULL,           'i' },
 		{ NULL,         0,                      NULL,           0 }
 	};
 
-	while ((ch = getopt_long(argc, argv, "h", longopts, NULL)) != -1)
+	while ((ch = getopt_long(argc, argv, "hdi:j:t:w:", longopts, NULL)) != -1)
 		switch (ch) {
+		case 'd':
+			opt_debug=1;
+			break;
+		case 'j':
+			opt_concurrent = strtoul(optarg, NULL, 10);
+			break;
+		case 't':
+			opt_max = strtoul(optarg, NULL, 10);
+			break;
+		case 'w':
+			opt_wait = strtoul(optarg, NULL, 10);
+			break;
 		case 'i':
 			opt_ifname = optarg;
 			break;
@@ -252,46 +293,83 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	rc = send_packet(packet_socket, 1);
-	{
-		struct dhcp_packet *r = rcv_buf;
-		struct dhcp_packet *s = snd_buf;
-		
-		cnt = 0;
-			
-		while(cnt++<1500 &&memcmp((void*)r->dhcp.addr, (void*)s->eth.ether_shost, ETH_ALEN)) {
-			memset(&sll, 0, sizeof(sll));  slen=sizeof(sll);
-			memset(rcv_buf, 0, sizeof(rcv_buf)); 
-			
-			if((rcnt=recvfrom(packet_socket, rcv_buf, sizeof(rcv_buf), 0,
-				(sa_t*)&sll, &slen))>0) {
-				
-					printf("%02x %d; ",r->eth.ether_dhost[5], r->ip.protocol);
-					fflush(stdout);
-					if(r->eth.ether_dhost[5]==0xff && r->ip.protocol==17) dump_packet(rcv_buf, rcnt);
-			} else {
-				perror("recvfrom");
-			}	
-		}
-		printf("Found it!: %d\n", rcnt);
+	session = calloc(opt_max, sizeof(session_t));
+	for(ndx=0; ndx<opt_max; ndx++) {
+		session[ndx].index = ndx;
+		session[ndx].state = SESS_START;
+		memset(session[ndx].client_mac_addr, '\xCC', ETH_ALEN);
+		session[ndx].client_mac_addr[5] = ndx;
 	}
 
-	rc = send_packet(packet_socket, 3);
-	{
-		struct dhcp_packet *r = rcv_buf;
-		struct dhcp_packet *s = snd_buf;
-		
-		cnt = 0;
-		while(cnt++<15 &&memcmp(r->dhcp.addr, s->eth.ether_shost, ETH_ALEN)) {
-		if((rcnt=recvfrom(packet_socket, rcv_buf, sizeof(rcv_buf), 0,
-			(sa_t*)&sll, &slen))>0) {
-			
-				printf("Got another: %d\n", rcnt);
-				dump_packet(rcv_buf, rcnt);
+	sessions_available = opt_concurrent;
+	sessions_pending   = opt_max;
+	sessions_remaining = opt_max;
+	ndx = 0;
+
+	while(sessions_pending) {
+		dhcp_t        *r = (dhcp_t *)rcv_buf;
+		unsigned char *bcast = "\xff\xff\xff\xff\xff\xff";
+		unsigned char *oui =   "\xcc\xcc\xcc";
+
+
+		while(sessions_available && sessions_remaining) {
+			if(session[ndx].state == SESS_START) {
+				rc = send_packet(packet_socket, session+ndx, 1);
+				session[ndx].state = SESS_DISCOVER;
+				ndx++;  ndx %= opt_max;
+				sessions_remaining--;
+				sessions_available--;
+				sleep(opt_wait);
 			}
-		}	
-		printf("Found it!: %d\n", rcnt);
+TRACE;
+		}
+		while(sessions_available==0 || (sessions_pending && sessions_remaining==0)) {
+TRACE;
+			memset(rcv_buf, 0, sizeof(rcv_buf)); 
+			while(memcmp((void*)r->dhcp.addr, oui, 3)) {
+TRACE;
+				memset(&sll, 0, sizeof(sll));  slen=sizeof(sll);
+				memset(rcv_buf, 0, sizeof(rcv_buf)); 
+				
+				if((rcnt=recvfrom(packet_socket, rcv_buf, sizeof(rcv_buf), 0,
+					(sa_t*)&sll, &slen))>0) {
+					
+TRACE;
+#if 0
+						printf("%x\n", memcmp(r->eth.ether_dhost, bcast, ETH_ALEN)==0);
+						printf("%x\n", (r->dhcp.id & 0xffff)==0xfeca);
+						printf("%x\n", (r->ip.protocol==17));
+						printf("%x\n", memcmp(r->dhcp.addr, oui, 3)==0);
+#endif
+						if( (memcmp(r->eth.ether_dhost, bcast, ETH_ALEN)==0) 
+							&& (r->ip.protocol==17 )
+							&& ((r->dhcp.id & 0xffff ) == 0xfeca)
+							&& (memcmp(r->dhcp.addr, oui,3)==0)) {
+							unsigned char found = r->dhcp.addr[5];
+DETAIL;
+	
+							switch(session[found].state) {
+							case SESS_DISCOVER:
+								rc = send_packet(packet_socket, session+found, 3);
+		
+								session[found].state = SESS_REQUEST;
+								break;
+							case SESS_REQUEST:
+								session[found].state = SESS_DONE;
+								sessions_pending--;
+								sessions_available++;
+								break;
+							}
+DETAIL;
+						}
+					//	dump_packet(rcv_buf, rcnt);
+				} else {
+					perror("recvfrom");
+				}	
+			}
+		}
 	}
+
 	close(packet_socket);
 
 	return(0);
